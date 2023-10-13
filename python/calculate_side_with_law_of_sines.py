bca477b55a48aa44bf2bb9172cfce33d347a1a56
Angelle <Angelles@users.noreply.github.com>
2023-10-13 11:55:37 -0400

    Update calculate_side_with_law_of_sines.py
    Refactored to include a loop and to move the initial prompt to its own function.

--- python/calculate_side_with_law_of_sines.py
+++ python/calculate_side_with_law_of_sines.py
@@ -9,28 +9,45 @@ import math
 ###
 
 
-def get_inputs():
-    print("The equation that this problem solves takes the following structure:\n"
-          "sin(A)/a = sin(C)/c.\nWe are solving for c.\nNote that sin(A)/a = sin(B)/b as well, "
-          "per the Law of Sines.\n\n")
-    angle_measure_1 = float(input("What is the measure of the first angle in degrees?\nEnter only numbers.\n"
-                                  "This question refers to the nominator on the left side of the equation.\n"))
-    side_measure = float(input("What is the length of the side corresponding to the angle you just entered?\n"
-                               "Enter only numbers.\n"
-                               "This question refers to the denominator on the left side of the equation.\n"))
-    angle_measure_2 = float(input("What is the measure of the second angle in degrees?\n Enter only numbers.\n"
-                                  "This question refers to the nominator on the right side of the equation.\n"))
-    return angle_measure_1, side_measure, angle_measure_2
+def get_status():
+    intro = str(input("The equation that this problem solves takes the following structure:\n"
+                      "sin(A)/a = sin(C)/c.\nWe are solving for c.\nNote that sin(A)/a = sin(B)/b as well, "
+                      "per the Law of Sines.\n Enter n to quit or y to continue.\n"))
+    for i in range(1):
+            if intro == "y":
+                on = True
+            elif intro == "n":
+                on = False
+            elif intro != "y" or "n":
+                print("There is an issue!")
+    return intro
 
 
 def calculate_side():
-    inputs = get_inputs()
-    # Calculate the measure of the third angle.
-    angle_measure_3 = 180 - (inputs[0] + inputs[2])
-    print("The measure of the third angle:", angle_measure_3)
-    # Calculate the value of side c, using the law of sines, where sin A/a = sin B/b = sin C/c.
-    results = (math.sin(math.radians(inputs[2])) * inputs[1]) / math.sin(math.radians(inputs[0]))
-    print("The length of side c:", round(results, 1))
+    on = True
+    intro = get_status()
+    while on:
+        if intro[0] == "y":
+            # Get inputs.
+            angle_measure_1 = float(input("What is the measure of the first angle in degrees?\nEnter only numbers.\n"
+                                          "This question refers to the nominator on the left side of the equation.\n"))
+            side_measure_1 = float(input("What is the length of the side corresponding to the angle you just entered?\n"
+                                         "Enter only numbers.\n"
+                                         "This question refers to the denominator on the left side of the equation.\n"))
+            angle_measure_2 = float(input("What is the measure of the second angle in degrees?\n Enter only numbers.\n"
+                                          "This question refers to the nominator on the right side of the equation.\n"))
+            inputs = [angle_measure_1, side_measure_1, angle_measure_2]
+            # Calculate the measure of the third angle.
+            angle_measure_3 = (180.0 - (inputs[0] + inputs[2]))
+            print("The measure of the third angle:", angle_measure_3)
+            # Calculate the value of side c, using the law of sines, where sin A/a = sin B/b = sin C/c.
+            results = (math.sin(math.radians(inputs[2])) * inputs[1]) / (math.sin(math.radians(inputs[0])))
+            print("The length of the side we are looking for:", results)
+        elif intro[0] == "n":
+            on = False
+            print("Thank you for using the program!")
+        else:
+            print("An error occurred.")
 
 
 calculate_side()
