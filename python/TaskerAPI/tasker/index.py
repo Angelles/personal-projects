f8de068a4c3f74ecad7566c3a4ab40e5e89fa9bd
Angelle <Angelles@users.noreply.github.com>
2023-12-29 13:51:52 -0500

    Added more code to make new endpoints, but it's not working. Will investigate.

---
+++ python/TaskerAPI/tasker/index.py
@@ -0,0 +1,48 @@
+from flask import Flask, jsonify, request
+
+import model.hobby
+import model.school
+import model.task_type
+
+app = Flask(__name__)
+
+tasks = [
+    model.school.School('1', 'this is a body', 'not started', 'this is a title'),
+    model.hobby.Hobby('2', 'this is a body 2', 'not started', 'this is a title 2')
+    ]
+
+
+@app.route('/school')
+def get_schools():
+    schema = model.school.SchoolSchema(many=True)
+    schools = schema.dump(
+        filter(lambda t: t.type == model.task_type.TaskType.SCHOOL, tasks)
+    )
+    return jsonify(schools)
+
+
+@app.route('/school', methods=['POST'])
+def add_school():
+    school = model.school.SchoolSchema().load(request.get_json())
+    tasks.append(school)
+    return "", 204
+
+
+@app.route('/hobby')
+def get_hobbies():
+    schema = model.hobby.HobbySchema(many=True)
+    hobbies = schema.dump(
+        filter(lambda t: t.type == model.task_type.TaskType.HOBBY, tasks)
+    )
+    return jsonify(hobbies)
+
+
+@app.route('/hobby', methods=['POST'])
+def add_hobby():
+    hobby = model.hobby.HobbySchema().load(request.get_json())
+    tasks.append(hobby)
+    return "", 204
+
+
+if __name__ == "__main__":
+    app.run()
