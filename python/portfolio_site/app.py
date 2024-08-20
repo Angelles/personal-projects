ff4e5ef184fb29eac1c1a9be03abbc2bb12b20c3
Angelle <Angelles@users.noreply.github.com>
2024-08-20 08:41:20 -0400

    Add CSS file and rename index page

--- python/portfolio_site/app.py
+++ python/portfolio_site/app.py
@@ -5,12 +5,8 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def home():
-    return render_template('app.html')
-
-@app.route("/about")
-def about():
-    return render_template('about.html')
+def index():
+    return render_template('index.html')
 
 @app.get("/login")
 def login_get():
