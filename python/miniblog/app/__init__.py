68d885ca5acbe4b6d952e48e53740b95abe4f33a
Angelle <Angelles@users.noreply.github.com>
2024-08-20 15:52:11 -0400

    Add latest changes from ch. 3 tutorial.

--- python/miniblog/app/__init__.py
+++ python/miniblog/app/__init__.py
@@ -1,13 +1,9 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
-@app.get("/login")
-def login_get():
-    return show_the_login_form()
-
-@app.post("/login")
-def login_post():
-    return do_the_login()
 
 from app import routes
+
--- python/miniblog/app/routes.py
+++ python/miniblog/app/routes.py
@@ -1,8 +1,8 @@
-from app import app
 from flask import render_template
+from app import app
+
 
 @app.route('/')
-
 @app.route('/index')
 def index():
     user = {'username': 'Angelle'}  # placeholder user
