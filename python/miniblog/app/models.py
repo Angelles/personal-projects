5a14c6adde78620d34db936f5666a65a3fe7a4f4
Angelle Leger <Angelles@users.noreply.github.com>
2024-08-22 09:44:47 -0400

    Add password hashing and verification functions to models.py.

--- python/miniblog/app/models.py
+++ python/miniblog/app/models.py
@@ -3,6 +3,8 @@ from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 
 # this class represents users stored in the db
@@ -15,6 +17,13 @@ class User(db.Model):
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
     posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')  # creates db relationship between posts and author
+
+    # password hashing and verification
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 # defines how to print User class objects
     def __repr__(self):
