cc111b0856be18244847cde29fe57d7c7d2161e2
Angelle Leger <Angelles@users.noreply.github.com>
2024-08-22 09:49:36 -0400

    Updated models and checked password hashing and verification in shell.

--- python/miniblog/app/models.py
+++ python/miniblog/app/models.py
@@ -1,9 +1,10 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
-from werkzeug.security import generate_password_hash, check_password_hash
+
 
 
 
