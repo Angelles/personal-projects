c707669890a5efd698321ff62335b51418de7c40
Angelle <Angelles@users.noreply.github.com>
2024-08-24 12:02:36 -0400

    Add logout function and route.

--- python/miniblog/app/routes.py
+++ python/miniblog/app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, url_for
 from app import app, db
 from app.forms import LoginForm
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 import sqlalchemy as sa
 from app.models import User
 
@@ -26,7 +26,7 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index')) # redirect to index page if the user is logged in
+        return redirect(url_for('index'))  # redirect to index page if the user is logged in
     form = LoginForm()
     if form.validate_on_submit():
         user = db.session.scalar(
@@ -37,3 +37,8 @@ def login():
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
