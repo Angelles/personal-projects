732d8d8ff9ed610fa9625c0cb1292855d9b5c485
Angelle <Angelles@users.noreply.github.com>
2024-08-24 16:13:52 -0400

    Add changes relating to logging in and out and using the current user's info

--- python/miniblog/app/models.py
+++ python/miniblog/app/models.py
@@ -1,11 +1,20 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from app import db, login
+from flask_login import UserMixin
+
+
+# load a user from the db given the user id
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
 
 
 # this class represents users stored in the db
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
                                                 unique=True)
@@ -13,6 +22,30 @@ class User(db.Model):
                                              unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
-    # defines how to print User class objects
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')  # creates db relationship between posts and author
+
+    # password hashing and verification
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+# defines how to print User class objects
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+class Post(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
+                                               index=True)  # secondary key that maps to User id
+    author: so.Mapped[User] = so.relationship(back_populates='posts')
+
+    # defines how to print Post class objects
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+
+    
