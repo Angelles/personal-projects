8e22b6c3466ae296f6b392197c30fcb55dffc665
Angelle Leger <Angelles@users.noreply.github.com>
2024-09-23 09:47:18 -0400

    Add updates to app init file.

--- python/miniblog/tests.py
+++ python/miniblog/tests.py
@@ -3,13 +3,28 @@ os.environ['DATABASE_URL'] = 'sqlite://'
 
 from datetime import datetime, timezone, timedelta
 import unittest
-from app import app, db
+from app import create_app, db
 from app.models import User, Post
+from config import Config
 
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
 class UserModelCase(unittest.TestCase):
+    # def setUp(self):
+    #     self.app_context = app.app_context()
+    #     self.app_context.push()
+    #     db.create_all()
+
+    # def tearDown(self):
+    #     db.session.remove()
+    #     db.drop_all()
+    #     self.app_context.pop()
+
     def setUp(self):
-        self.app_context = app.app_context()
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
 
@@ -17,7 +32,6 @@ class UserModelCase(unittest.TestCase):
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
-
     def test_password_hashing(self):
         u = User(username='susan', email='susan@example.com')
         u.set_password('cat')
