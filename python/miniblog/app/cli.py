734007c12d8bc5f09a065365db5cc4789f4bd8ef
Angelle <Angelles@users.noreply.github.com>
2024-09-27 19:18:12 -0400

    Updated db.

--- python/miniblog/app/cli.py
+++ python/miniblog/app/cli.py
@@ -1,36 +1,15 @@
-from flask import Blueprint
 import os
+from flask import Blueprint
 import click
-
 
 bp = Blueprint('cli', __name__, cli_group=None)
 
-
-# from https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-xiii-i18n-and-l10n#:~:text=Command%2DLine%20Enhancements
 
 @bp.cli.group()
 def translate():
     """Translation and localization commands."""
     pass
 
-# flask translate update to update all language repositories
-@translate.command()
-def update():
-    """Update all languages."""
-    if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
-        raise RuntimeError('extract command failed')
-    if os.system('pybabel update -i messages.pot -d app/translations'):
-        raise RuntimeError('update command failed')
-    os.remove('messages.pot')
-
-# flask translate compile to compile all language repositories
-@translate.command()
-def compile():
-    """Compile all languages."""
-    if os.system('pybabel compile -d app/translations'):
-        raise RuntimeError('compile command failed')
-
-# flask translate init LANG to add a new language
 
 @translate.command()
 @click.argument('lang')
@@ -42,3 +21,20 @@ def init(lang):
             'pybabel init -i messages.pot -d app/translations -l ' + lang):
         raise RuntimeError('init command failed')
     os.remove('messages.pot')
+
+
+@translate.command()
+def update():
+    """Update all languages."""
+    if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
+        raise RuntimeError('extract command failed')
+    if os.system('pybabel update -i messages.pot -d app/translations'):
+        raise RuntimeError('update command failed')
+    os.remove('messages.pot')
+
+
+@translate.command()
+def compile():
+    """Compile all languages."""
+    if os.system('pybabel compile -d app/translations'):
+        raise RuntimeError('compile command failed')
--- python/miniblog/app/models.py
+++ python/miniblog/app/models.py
@@ -10,12 +10,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 from app import db, login
 
-# load a user from the db given the user id
-@login.user_loader
-def load_user(id):
-    return db.session.get(User, int(id))
 
-# followers association table
 followers = sa.Table(
     'followers',
     db.metadata,
@@ -25,7 +20,7 @@ followers = sa.Table(
               primary_key=True)
 )
 
-# this class represents users stored in the db
+
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
@@ -33,31 +28,12 @@ class User(UserMixin, db.Model):
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
                                              unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-
-    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')  # creates db relationship between posts and author
-
     about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
-    
     last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
         default=lambda: datetime.now(timezone.utc))
 
-    # password hashing and verification
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    # defines how to print User class objects
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
-    
-    # generate and return avatar for each user
-    def avatar(self, size):
-            digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-            return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
-    
-    # many to many relationships: user <-> following, user <-> followers
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(
+        back_populates='author')
     following: so.WriteOnlyMapped['User'] = so.relationship(
         secondary=followers, primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
@@ -67,7 +43,19 @@ class User(UserMixin, db.Model):
         secondaryjoin=(followers.c.follower_id == id),
         back_populates='following')
 
-    # follow/unfollow functions - includes counting how many following/followers
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+
     def follow(self, user):
         if not self.is_following(user):
             self.following.add(user)
@@ -89,8 +77,7 @@ class User(UserMixin, db.Model):
         query = sa.select(sa.func.count()).select_from(
             self.following.select().subquery())
         return db.session.scalar(query)
-    
-    # get posts of the users the current user follows
+
     def following_posts(self):
         Author = so.aliased(User)
         Follower = so.aliased(User)
@@ -109,16 +96,22 @@ class User(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256')
+            current_app.config['SECRET_KEY'], algorithm='HS256')
 
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config['SECRET_KEY'],
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
                             algorithms=['HS256'])['reset_password']
-        except:
+        except Exception:
             return
         return db.session.get(User, id)
+
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -126,11 +119,10 @@ class Post(db.Model):
     timestamp: so.Mapped[datetime] = so.mapped_column(
         index=True, default=lambda: datetime.now(timezone.utc))
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
-                                               index=True)  # secondary key that maps to User id
-    author: so.Mapped[User] = so.relationship(back_populates='posts')
-    # language: so.Mapped[Optional[str]] = so.mapped_column(sa.String(5))  # field for detected language
+                                               index=True)
+    language: so.Mapped[Optional[str]] = so.mapped_column(sa.String(5))
 
-    # defines how to print Post class objects
+    author: so.Mapped[User] = so.relationship(back_populates='posts')
+
     def __repr__(self):
         return '<Post {}>'.format(self.body)
-
