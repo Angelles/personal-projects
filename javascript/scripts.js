d8624c2fa9b3378c69c2ab39f51036ae08424918
Angelle <Angelles@users.noreply.github.com>
2025-02-24 08:40:21 -0500

    Create scripts.js

---
+++ javascript/scripts.js
@@ -0,0 +1,10 @@
+/* This is a placeholder file. */
+
+/* Button actions - includes on click and hover actions */
+// Submit contact form button
+
+// Send email button
+
+// Call company number button 
+
+/* Consider a carousel of photos */
